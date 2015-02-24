1addd619c7b5ada2f7c69b712f0a3072aae429d9
InterGenOS <chris@intergenstudios.com>
2015-02-24 02:32:29 -0600

    Update 5. InitialSetup.sh

--- 5. InitialSetup.sh
+++ 5. InitialSetup.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
 #############################################################################################
-##******!!!!        Make sure to set your '/dev/sdX' in the script below         !!!!******##
-##******!!!! Also note that the user 'igos' is being given the pass 'intergenos' !!!!******##
+##****!!!!  Please note that the user 'igos' is being given the pass 'intergenos'  !!!!****##
 #############################################################################################
+
+echo "Welcome to InterGenOS Initial Setup."
+echo " "
+echo "Please enter the partition identifier where you'd like to build InterGenOS"
+echo " "
+read -p "We should set it up for you in /dev/: "
+echo " "
+read -p "Awesome, we'll set up the build for you in /dev/$REPLY, is that alright? [y/n] : " opt
+        if [ $opt != y ]; then
+                echo " "
+                echo "Oh," 
+                sleep 1
+                echo "snap,"
+                sleep 1
+                echo "Well, then let's make sure we've got your build partition set right, ok?"
+                echo " "
+                read -p "You want it set up on /dev/: " REPLY2
+                echo " "
+                read -p "I think we've go it now- you want it set up in /dev/$REPLY2, right? [y/n] : " opt2
+                        if [ $opt2 != y ]; then
+                                echo " "
+                                echo "Hmmm...Ok, you should double check your build partition info and then run the Initial Setup again."
+                                sleep 1
+                                echo " "
+                                echo "We'll hang out right here 'till you get back.  :) "
+                                sleep 1
+                                echo " "
+                                exit 0
+                        else
+                                echo "Thank you, the build will now proceed"
+                        fi
+        else
+                echo "Thank you, the build will now proceed"
+        fi
+sleep 1
+echo "You will be prompted for manual entries when needed."
+sleep 1
+echo "We appreciate your participation in the InterGenOS project."
+echo " "
+echo " "
 
 mkdir -pv $IGos
 
-mount -v -t ext4 /dev/sdX $IGos &&
+if [ $opt = y ]; then
+    mount -v -t ext4 /dev/$REPLY $IGos &&
+else
+    mount -v -t ext4 /dev/$REPLY2 $IGos &&
+fi
 
 mkdir -v $IGos/sources
 
