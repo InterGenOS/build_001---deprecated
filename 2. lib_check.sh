40ba6854b291ccd102ef8353641a1d3ad751557d
InterGenOS <chris@intergenstudios.com>
2015-02-24 00:42:12 -0600

    Update 2. lib_check.sh

--- 2. lib_check.sh
+++ 2. lib_check.sh
@@ -1,3 +1,9 @@
+#********************************************************************************************************#
+#*** Until the installer has been completed, the initial setup for the builds has to be done manually ***#
+#*** The scripts provided make the core system build fairly automated, and scripts are being written  ***#
+#*** to automate the extended packages as well.  Keep checking back for updated scripts.              ***#
+#********************************************************************************************************#
+
 #!/bin/bash
 for lib in lib{gmp,mpfr,mpc}.la; do
   echo $lib: $(if find /usr/lib* -name $lib|
