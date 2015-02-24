73894447c61c74a95971cfeba558d38d09efb0c0
InterGenOS <chris@intergenstudios.com>
2015-02-24 13:30:18 -0600

    Update 5. InitialSetup.sh

--- 5. InitialSetup.sh
+++ 5. InitialSetup.sh
@@ -13,24 +13,20 @@ NC="$(echo -e "\033[0m")"
 ### Python heading
 function print_heading {
 python - <<END
-
 ### Python variables
 import time
 import sys
-
 ### Python functions
 def delay_print(s):
     for c in s:
         sys.stdout.write( '%s' % c )
         sys.stdout.flush()
         time.sleep(0.003)
-
 def delay_print2(s):
     for c in s:
         sys.stdout.write( '%s' % c )
         sys.stdout.flush()
         time.sleep(0.4)
-
 print(" ")
 print(" ")
 delay_print('${lblue}*************************************************************${NC}')
@@ -44,10 +40,9 @@ print(" ")
 delay_print("${lblue}|${NC}                       ${green}Build${NC}_${green}001${NC}                           ${lblue}|${NC}")
 print(" ")
 delay_print("${lblue}|                                                           |${NC}")
-print(" ")                                                                                                                                                                                    
+print(" ")
 delay_print("${lblue}*************************************************************${NC}")
 print(" ")
-
 END
 }
 
@@ -62,55 +57,72 @@ echo " "
 echo " "
 echo "${green}Please enter the ${NC}Partition ID ${green}where you'd like to build ${NC}Inter${lblue}Gen${NC}OS"
 echo " "
-read -p "${green}We should set it up for you in /dev/${NC}?${green}:${NC} "
+read -p "${green}Set it up for you in /dev/${NC}?${green}:${NC} "
 echo " "
-read -p "${green}Ok, you want the build set up for you in /dev/${NC}$REPLY${green}, correct? ${NC}[y/n] ${green}:${NC} " opt
-        if [ $opt != y ]; then
-                echo " "
-                echo -e "${red}Oh,${NC}" 
-                sleep 1
-                echo " "
-		echo -e "${red}snap,${NC}"
-                sleep 1
-                echo " "
-		echo -e "${green}Well, then let's make sure we've got your ${NC}Build Partition ID ${green}set right, ok?${NC}"
-                echo " "
-                read -p "${green}You want it set up on /dev/${NC}?${green}:${NC} " REPLY2
-                echo " "
-                read -p "${green}Alright, let's double check- you want your build set up in /dev/${NC}$REPLY2${green}, right? ${NC}[y/n] ${green}:${NC} " opt2
-                        if [ $opt2 != y ]; then
-                                echo " "
-				echo " "
-                                echo -e "${NC}Hmmm...${lblue}Ok, you should double check your ${NC}Build Partition ID ${lblue}and then run the ${NC}Initial Setup ${lblue}again.${NC}"
-                                sleep 1
-                                echo " "
-                                echo -e "${green}We'll hang out right here 'till you get back.  ${NC}:) "
-                                sleep 1
-                                echo " "
-                                exit 0
-                        else
-                                echo -e "${green}Thank you, the build will now proceed${NC}"
-                        fi
-        else
-		echo " "
-                echo -e "${green}Thank you, the build will now proceed${NC}"
-        fi
-sleep 1
+echo "${green}Ok, target build ${NC}Partition ID ${green}is /dev/${NC}$REPLY"
 echo " "
-echo -e "${green}You will be prompted for manual entries when needed.${NC}"
-sleep 1
-echo " "
-echo -e "${green}We appreciate your participation in the InterGenOS project.${NC}"
-sleep 1
-echo " "
-echo " "
-
-mkdir -pv $IGos
+read -p "${green}Ready to begin${NC}? [y/n] ${green}:${NC} " opt
 
 if [ $opt = y ]; then
-    mount -v -t ext4 /dev/$REPLY $IGos &&
+    echo " "
+    echo -e "${green}Thank you, the build will now proceed${NC}"
+    sleep 1
+    echo " "
+    echo -e "${green}You will be prompted for manual entries when needed.${NC}"
+    sleep 1
+    echo " "
+    echo -e "${green}We appreciate your participation in the InterGenOS project.${NC}"
+    sleep 1
+    echo " "
+    echo " "
+    mkdir -pv $IGos
+    mount -v -t ext4 /dev/$REPLY $IGos
 else
-    mount -v -t ext4 /dev/$REPLY2 $IGos &&
+    echo " "
+    echo -e "${red}Oh,${NC}"
+    sleep 1
+    echo " "
+    echo -e "${red}snap,${NC}"
+    sleep 1
+    echo " "
+    echo -e "${green}Well, then let's make sure we've got your ${NC}Build Partition ID ${green}set right, ok?${NC}"
+    echo " "
+    read -p "${green}Which ${NC}Partition ID ${green}did you want it set up on- /dev/${NC}?${green}:${NC} " REPLY2
+    echo " "
+    echo " "
+    echo -e "${green}Alright, target build ${NC}Partition ID ${green}is set to /dev/${NC}$REPLY2"
+    echo " "
+    read -p "${green}Ready to begin${NC}? [y/n] ${green}:${NC} " opt2
+    if [ $opt2 = y ]; then
+        echo " "
+        echo -e "${green}Thank you, the build will now proceed${NC}"
+        sleep 1
+        echo " "
+        echo -e "${green}You will be prompted for manual entries when needed.${NC}"
+        sleep 1
+        echo " "
+        echo -e "${green}We appreciate your participation in the InterGenOS project.${NC}"
+        sleep 1
+        echo " "
+        echo " "
+        mkdir -pv $IGos
+        mount -v -t ext4 /dev/$REPLY2 $IGos
+    else
+        echo " "
+        echo -e "${red}Oh,${NC}"
+        sleep 1
+        echo " "
+        echo -e "${red}snap,${NC}"
+        sleep 1
+        echo " "
+        echo -e "${NC}Hmmm...${lblue}Ok, you should double check your ${NC}Build Partition ID ${lblue}and then run the ${NC}Initial Setup ${lblue}again.${NC}"
+        sleep 1
+        echo " "
+        echo -e "${green}We'll hang out right here 'till you get back.  ${NC}:) "
+        sleep 1
+        echo " "
+        exit 0
+    fi
 fi
 
 mkdir -v $IGos/sources
@@ -135,10 +147,6 @@ useradd -s /bin/bash -g igos -m -k /dev/null igos
 
 echo "igos:intergenos" | chpasswd &&
 
-chmod +x $IGos/SetEnv.sh
-
-chmod +x $IGos/Build1stPass.sh
-
 chown -v igos $IGos/tools
 
 chown -v igos $IGos/sources
@@ -146,6 +154,10 @@ chown -v igos $IGos/sources
 wget http://intergenstudios.com/Downloads/SetEnv.sh -P $IGos
 
 wget http://intergenstudios.com/Downloads/Build1stPass.sh -P $IGos
+
+chmod +x $IGos/SetEnv.sh
+
+chmod +x $IGos/Build1stPass.sh
 
 chown -v igos $IGos/SetEnv.sh
 
